7631640b7a0eb6ac0f37b53452dad10d72e3114e
MariusBerton <berton.marius@gmail.com>
2024-10-18 08:50:51 +0200

    Update Cards.py

--- Cards.py
+++ Cards.py
@@ -4,3 +4,18 @@ from Player import Player
 class Cards:
     def __init__(self, name: str) -> None:
         self.name = name
+
+    
+    def pick(self):
+        #use a card
+        pass
+
+
+    def discard(self):
+        #the card go to the discard pile
+        pass
+
+
+    def draw(self):
+        #draw a card
+        pass
